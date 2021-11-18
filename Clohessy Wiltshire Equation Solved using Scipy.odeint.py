1c84d8fa51271e1dade7aa70edca6b2a0ca04282
byang12159 <92831530+byang12159@users.noreply.github.com>
2021-11-18 10:44:09 -0600

    Add files via upload
    Check ODE solver

---
+++ Clohessy Wiltshire Equation Solved using Scipy.odeint.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+# Import the required modules
+import numpy as np
+import matplotlib.pyplot as plt
+# This makes the plots appear inside the notebook
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[90]:
+
+
+from scipy.integrate import odeint
+
+miu = 3.986*10**14
+a = 6793137
+#n = np.sqrt(miu/(a**3))
+n = 10
+# Define a function which calculates the derivative
+def dU_dx(U, x):
+    return [U[1],   3*(n**2)*U[0]+2*n*U[3],   U[3],   -2*n*U[1],     U[5],      -(n**2)*U[4]]
+
+U0 = [0,0,0,1,1,1]
+xs = np.linspace(0,10,1000)
+
+Us = odeint(dU_dx, U0, xs)
+xval = Us[:,0] #first column x-coordinates
+yval = Us[:,2]
+zval = Us[:,4]
+print(Us)
+
+
+# In[91]:
+
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')  
+ax.set_aspect('auto')
+ax.plot3D(xval,yval,zval, 'grey')
+
+
+# In[82]:
+
+
+# Plot the numerical solution
+plt.rcParams.update({'font.size': 14})  # increase the font size
+plt.xlabel("x")
+plt.ylabel("y")
+plt.plot(xval, yval);
+
+
+# In[70]:
+
+
+plt.plot(yval,zval);
+
+
+# In[77]:
+
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+theta = np.linspace( 0 , 2 * np.pi , 150 )
+ 
+radius = 0.4
+ 
+a = radius * np.cos( theta )
+b = radius * np.sin( theta )
+ 
+figure, axes = plt.subplots( 1 )
+ 
+axes.plot( a, b )
+axes.set_aspect( 1 )
+
+plt.show()
+
